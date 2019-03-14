84227343f50d0be8981511c17bced4bc18f2578d
rynno <siin.mina@mail.ee>
2019-03-14 13:34:09 +0200

    added rest controller

---
+++ eureka-discovery/src/main/java/eu/babywatcher/ServiceInstanceRestController.java
@@ -0,0 +1,15 @@
+package eu.babywatcher;
+
+
+@RestController
+class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
